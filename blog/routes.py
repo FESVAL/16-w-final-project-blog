ef821e465174f6c4014b7bd2ea502b7e7b6c8641
Valentyna Feshchenko <fesval777@gmail.com>
2023-03-26 19:08:52 +0200

    commit_8

--- blog/routes.py
+++ blog/routes.py
@@ -3,12 +3,13 @@ from faker import Faker
 from blog import app
 #from . import app   
 
+from blog.models import Entry, db
+from blog.forms import EntryForm
 #from .models import Entry
 #from . import db
 #from .forms import EntryForm
-from blog.models import Entry, db
-from blog.forms import EntryForm
 
+#функция фейковых записей
 
 #def generate_entries(how_many=10):
     #fake = Faker()
@@ -21,15 +22,18 @@ from blog.forms import EntryForm
         #)
         #db.session.add(post)
     #db.session.commit()
+    #all_posts = Entry.query.all()
+    #return all_posts
+
 
 @app.route("/")
 def index():
    all_posts = Entry.query.filter_by(is_published=True).order_by(Entry.pub_date.desc())
    return render_template("homepage.html", all_posts=all_posts)
 
-
-@app.route("/new-post/", methods=["GET", "POST"])
-def create_entry():
+#окрема функція створення нового запису - видалити після об'єднання
+#@app.route("/new-post/", methods=["GET", "POST"])
+#def create_entry():
    form = EntryForm()
    errors = None
    if request.method == 'POST':
@@ -45,8 +49,9 @@ def create_entry():
            errors = form.errors
    return render_template("entry_form.html", form=form, errors=errors)
 
-@app.route("/edit-post/<int:entry_id>", methods=["GET", "POST"])
-def edit_entry(entry_id):
+#окрема функція коригування запису - видалити після об'єднання
+#@app.route("/edit-post/<int:entry_id>", methods=["GET", "POST"])
+#def edit_entry(entry_id):
    entry = Entry.query.filter_by(id=entry_id).first_or_404()
    form = EntryForm(obj=entry)
    errors = None
@@ -58,3 +63,37 @@ def edit_entry(entry_id):
            errors = form.errors
    return render_template("entry_form.html", form=form, errors=errors)
 
+#!!!завдання - спільна функція створення та редагування запису 
+@app.route("/posts/<int:entry_id>", methods=["GET", "POST"])
+def create_edit_entry(entry_id):
+   errors = None
+
+   if entry_id==0: #создаем новую запись
+       
+    form = EntryForm()
+    if request.method == 'POST':
+       if form.validate_on_submit():
+           entry = Entry(
+               title=form.title.data,
+               body=form.body.data,
+               is_published=form.is_published.data,
+           )
+           db.session.add(entry)
+           db.session.commit()
+       else:
+           errors = form.errors
+   
+
+    else:  # редактируем старую запись
+       entry = Entry.query.filter_by(id=entry_id).first_or_404()
+       form = EntryForm(obj=entry)
+   
+       if request.method == 'POST':
+            if form.validate_on_submit():
+                form.populate_obj(entry)
+                db.session.commit()
+            else:
+                errors = form.errors
+
+
+   return render_template("entry_form.html", form=form, errors=errors)
